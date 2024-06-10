4cf1f9187cd7e63b2da63be20218d43fbc2e5fe3
KhushaliVaran <150055063+KhushaliVaran@users.noreply.github.com>
2024-06-10 09:50:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-package banktransactions;
+package methodOverriding;
 
-public class Main 
-{
-	public static void main(String[] args)
+public class Main {
+
+	public static void main(String[] args) 
 	{
-		SavingsAcc savings = new SavingsAcc(1000);
-		CheckAcc check = new CheckAcc(500);
-		Transaction trans = new Transaction(5.00);
+		Child ch = new Child();
+		ch.sayHello("Hey There!!");
 		
-		trans.performTransaction(savings,200,true); // deposit in savings
-		trans.performTransaction(check,100,false); // Withdraw from checking
+		Parent pr = new Parent();
+		pr.sayHello("Have a Good Day!!");
 		
-		System.out.println("Total accounts created: " + Bank.getTotalAccounts());
 	}
+
 }
